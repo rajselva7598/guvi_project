6552e0f524ab2cfec21b7a67ccf55296b2b23373
rajselva7598 <38922527+rajselva7598@users.noreply.github.com>
2022-06-06 15:55:32 +0530

    final commit

--- Guvi project/first.py
+++ Guvi project/first.py
@@ -1,82 +1,93 @@
+import csv
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-
-class Login:
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.select import Select
+driver = webdriver.Chrome(executable_path=r"C:\Users\user\Downloads\chromedriver_win32\chromedriver.exe")
+class Create_query:
     '''Create the class for login page'''
-    service = Service('C:\Program Files (x86)\chromedriver.exe')
-    driver = webdriver.Chrome(service= service)
-    def provide_input(self,url):
-         #To provide the username and password for the login form
-        self.driver.get(url)
-        input_value = self.driver.find_element(
-        by=By.XPATH ,
-        value='/html/body/div/div/div/div[1]/div[2]/div/div[1]/form/div[1]/div/input')
-        input_value.send_keys('psra7598@gmail.com')
+    def login(self):
+        # To provide the username and password for the login form
+        driver.get('https://www.zenclass.in')
         time.sleep(4)
-        input_value = self.driver.find_element(
-        by=By.XPATH , 
-        value='/html/body/div/div/div/div[1]/div[2]/div/div[1]/form/div[2]/div/input')
-        input_value.send_keys('Varshini@4141013')
-        submit=self.driver.find_element(
-        by=By.XPATH , 
-        value='/html/body/div/div/div/div[1]/div[2]/div/div[1]/form/button')
-        submit.click()
-    def load_side_bar(self):
-        #loading the navigation bar
+        input_username = driver.find_element(by=By.XPATH,
+                                             value='//*[@id="root"]/div/div/div[1]/div[2]/div/div[1]/form/div[1]/div/input').send_keys('psra7598@gmail.com')
+        time.sleep(4)
+        input_userpassword = driver.find_element(by=By.XPATH,
+                                                 value='//*[@id="root"]/div/div/div[1]/div[2]/div/div[1]/form/div[2]/div/input').send_keys('Varshini@4141013')
+        time.sleep(4)
+        btn_login = driver.find_element(by=By.XPATH,
+                                        value='//*[@id="root"]/div/div/div[1]/div[2]/div/div[1]/form/button').click()
+     def main_page(self):
+        # loading the navigation bar
         time.sleep(3)
-        action = ActionChains(self.driver)
+        side_bar = driver.find_element(by=By.XPATH,
+                                       value='//*[@id="root"]/div[1]/nav/ul/div[1]/li/span/div/div[1]')
+        actionchains = ActionChains(driver)
         time.sleep(3)
-        action_main= self.driver.find_element(
-        by=By.XPATH , 
-        value='/html/body/div/div[1]/nav/ul/div[6]/li/span/div/div[2]')
+        actionchains.move_to_element(side_bar).perform()
         time.sleep(3)
-        action.move_to_element(action_main).click().perform()
-    def create_query(self ,url1):
-         #creating a query
+        create_Query = driver.find_element(by=By.XPATH,
+                                           value='//*[@id="root"]/div[1]/nav/ul/div[6]/li/span/div/div[2]')
         time.sleep(3)
-        self.driver.get(url1)
+        create_Query.click()
         time.sleep(3)
-        category =self.driver.find_element(
-        by=By.XPATH , 
-        value='//*[@id="root"]/div[2]/div/div[2]/div/div/form/div[2]/div[1]/select')
-        time.sleep(3)
-        communication =self.driver.find_element(
-        by=By.XPATH , 
-        value='//*[@id="root"]/div[2]/div/div[2]/div/div/form/div[2]/div[2]/select')
-        time.sleep(3)
-        query_title =self.driver.find_element(
-        by=By.XPATH , 
-        value='//*[@id="root"]/div[2]/div/div[2]/div/div/form/div[5]/div/input')
-        time.sleep(3)
-        query_description =self.driver.find_element(
-        by=By.XPATH ,
-        value='//*[@id="root"]/div[2]/div/div[2]/div/div/form/div[5]/textarea')
-        time.sleep(3)
-        btn_create_quries =self.driver.find_element(
-        by=By.XPATH , 
-        value='//*[@id="root"]/div[2]/div/div[2]/div/div/form/div[13]/div/button')
-        time.sleep(3)
-        category.send_keys("Placement Related")
-        time.sleep(3)
-        communication.send_keys('Tamil')
-        time.sleep(3)
-        query_title.send_keys('Guvi Python AT - 1 & W Automation Project')
-        time.sleep(3)
-        query_description.send_keys('This is a Project Test Code Running for the Python Automation - 1 & 2 Project Given by a mentor Mr.Suman Gangopadhyay')
-        time.sleep(3)
-        btn_create_quries.click()
-        self.driver.close()
-lurl = Login()
-#initial page
-URL='https://www.zenclass.in'
-#login credentials
-lurl.provide_input(URL)
-#navigation
-lurl.load_side_bar()
-#create queries
-URL1 ='https://www.zenclass.in/queries/create'
-lurl.create_query(URL1)
+        away_side_bar = driver.find_element(by=By.XPATH,
+                                            value="/html/body/div")
+        actionchains.move_to_element(away_side_bar).perform()
+    def create_Query_page(self):
+        # creating a query
+        numbers =0
+        while numbers < 4:
+            time.sleep(3)
+            btn_create_query = driver.find_element(by=By.XPATH,
+                                                   value='//*[@id="root"]/div[2]/div/div[1]/div[1]/button').click()
+            time.sleep(3)
+            btn_create_query_cancel = driver.find_element(by=By.XPATH,
+                                                          value="/html/body/div/div[2]/div/div[2]/div[6]/div[2]/div/div/section[3]/div[2]/button[1]").click()
+            time.sleep(3)
+            crete_query_category = driver.find_element(by=By.XPATH,
+                                                       value='//*[@id="root"]/div[2]/div/div[2]/div/div/form/div[2]/div[1]/select')
+            crete_query_category.click()
+            click_dd = Select(crete_query_category)
+            print(click_dd.select_by_index(1))
+            click_dd.select_by_index(1)  # zen class creating the query
+            time.sleep(3)
+            create_query_subcategory = driver.find_element(by=By.XPATH,
+                                                           value='//*[@id="root"]/div[2]/div/div[2]/div/div/form/div[2]/div[2]/select')
+            create_query_subcategory.click()
+            time.sleep(3)
+            click_dd = Select(create_query_subcategory)
+            time.sleep(3)
+            click_dd.select_by_index(1)  # zen class creating the subcategory
 
+            create_query_lang = driver.find_element(by=By.XPATH,
+                                                    value='//*[@id="root"]/div[2]/div/div[2]/div/div/form/div[2]/div[4]/select')
+            create_query_lang.click()
+            time.sleep(3)
+            click_dd = Select(create_query_lang)
+            time.sleep(3)
+            click_dd.select_by_index(2)  # zen class creating the subcategory
+
+            time.sleep(3)
+            create_query_Title = driver.find_element(by=By.XPATH,
+                                                     value='//*[@id="root"]/div[2]/div/div[2]/div/div/form/div[5]/div/input').send_keys(
+                'Guvi Python AT - 1 & W Automation Project')  # zen class for providing the title of the query
+
+            time.sleep(3)
+            create_query_description = driver.find_element(by=By.XPATH,
+                                                           value='//*[@id="root"]/div[2]/div/div[2]/div/div/form/div[5]/textarea').send_keys(
+                'This is a Project Test Code Running for the Python Automation - 1 & 2 Project Given by a mentor Mr.Suman Gangopadhyay')  # zen class for creating the description
+            time.sleep(3)
+            btn_submit = driver.find_element(by=By.XPATH,
+                                             value='//*[@id="root"]/div[2]/div/div[2]/div/div/form/div[13]/div/button').click()
+            numbers=numbers+1
+
+
+
+lurl = Create_query()           #object for the class
+lurl.login()
+lurl.main_page()
+lurl.create_Query_page()
